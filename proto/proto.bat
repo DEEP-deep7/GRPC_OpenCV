b95001a64e0045eabd2548076f6eea47c7e2d1a1
辜磊 <gulei@brainsmatics.org>
2021-09-04 09:28:56 +0800

    add gitignore

--- proto/proto.bat
+++ proto/proto.bat
@@ -1,3 +1,3 @@
-protoc -I. --grpc_out=. --plugin=protoc-gen-grpc="F:\\GRPCCpp\\build\\Release\\grpc_cpp_plugin.exe" ImageStream.proto
+protoc -I. --grpc_out=. --plugin=protoc-gen-grpc="D:\\GRPC\\grpcBuild\\Release\\grpc_cpp_plugin.exe" ImageStream.proto
 protoc --cpp_out=./ ImageStream.proto
 pause
